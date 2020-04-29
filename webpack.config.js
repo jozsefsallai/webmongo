b36d1721ab787de06a84b5de86490a8066c10bd4
József Sallai <jozsef@sallai.me>
2020-04-29 16:36:27 +0300

    use webpack instead of rollup

---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+const webpack = require('webpack');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+const path = require('path');
+const config = require('sapper/config/webpack.js');
+const pkg = require('./package.json');
+
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
+
+const alias = {
+  svelte: path.resolve('node_modules', 'svelte'),
+  '@': path.resolve('src')
+};
+const extensions = ['.mjs', '.js', '.json', '.svelte', '.html'];
+const mainFields = ['svelte', 'module', 'browser', 'main'];
+
+module.exports = {
+  client: {
+    entry: config.client.entry(),
+    output: config.client.output(),
+    resolve: { alias, extensions, mainFields },
+    module: {
+      rules: [
+        {
+          test: /\.(svelte|html)$/,
+          use: {
+            loader: 'svelte-loader',
+            options: {
+              dev,
+              emitCss: true,
+              hydratable: true,
+              hotReload: false // pending https://github.com/sveltejs/svelte/issues/2377
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCSSExtractPlugin.loader,
+            'css-loader'
+          ]
+        }
+      ]
+    },
+    mode,
+    plugins: [
+      // pending https://github.com/sveltejs/svelte/issues/2377
+      // dev && new webpack.HotModuleReplacementPlugin(),
+      new webpack.DefinePlugin({
+        'process.browser': true,
+        'process.env.NODE_ENV': JSON.stringify(mode)
+      }),
+      new MiniCSSExtractPlugin('[name].css')
+    ].filter(Boolean),
+    devtool: dev && 'inline-source-map'
+  },
+
+  server: {
+    entry: config.server.entry(),
+    output: config.server.output(),
+    target: 'node',
+    resolve: { alias, extensions, mainFields },
+    externals: Object.keys(pkg.dependencies).concat('encoding'),
+    module: {
+      rules: [
+        {
+          test: /\.(svelte|html)$/,
+          use: {
+            loader: 'svelte-loader',
+            options: {
+              css: false,
+              generate: 'ssr',
+              dev
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'file-loader',
+            'css-loader'
+          ]
+        }
+      ]
+    },
+    mode: process.env.NODE_ENV,
+    performance: {
+      hints: false // it doesn't matter if server.js is large
+    }
+  },
+
+  serviceworker: {
+    entry: config.serviceworker.entry(),
+    output: config.serviceworker.output(),
+    mode: process.env.NODE_ENV
+  }
+};
